c5153139483f1e2f4f2c285f4ee4fc69ff4eb114
Matt Dietz <matt.dietz@rackspace.com>
2014-02-05 00:50:57 -0600

    Trying to spit out a lib instead

--- Makefile
+++ Makefile
@@ -1,8 +1,23 @@
 CC=g++
 CFLAGS=-I./include -lsfml-window -lsfml-system -lsfml-graphics -lsfml-audio -wALL -ljson -Qunused-arguments
 # -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/usr/include/python2.7
-SOURCES=src/player.cpp src/scoundrel.cpp src/entity.cpp src/rect.cpp src/tile.cpp src/camera.cpp src/animation.cpp src/collidable.cpp src/game_map.cpp src/level_loader.cpp
+LIB_DIR=lib/
+SOURCE_DIR=src/
+OBJ_DIR=obj/
+SOURCE_FILES=player.cpp scoundrel.cpp entity.cpp rect.cpp tile.cpp camera.cpp animation.cpp collidable.cpp game_map.cpp level_loader.cpp
+SOURCES=$(SOURCE_FILES:%.cpp=$(SOURCE_DIR)%.cpp)
+OBJS=$(SOURCE_FILES:%.cpp=obj/%.o)
+LIB_FILE=libscoundrel.a
 
-all:
-	mkdir -p ./bin
-	$(CC) -o ./bin/scoundrel $(CFLAGS) $(SOURCES)
+all: $(LIB_FILE)
+
+obj:
+	mkdir -p $(OBJ_DIR)
+
+$(OBJS): $(SOURCES) obj
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIB_FILE): $(OBJS)
+	mkdir -p $(LIB_DIR)
+	$(CC) -c $(CFLAGS) $(OBJS) -fPIC
+	$(CC) -shared -o $(LIB_DIR)$(LIB_FILE) $(OBJS) $(CFLAGS)
--- src/scoundrel.cpp
+++ src/scoundrel.cpp
@@ -1,49 +1,6 @@
-#include <fstream>
-#include <json/json.h>
-#include <iostream>
-#include <list>
-#include <map>
-#include <sstream>
-#include <string>
-#include <stdlib.h>
-#include <unistd.h>
-
-//#include <Python.h>
-
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-
-#include "animation.h"
-#include "collidable.h"
-#include "core.h"
-#include "camera.h"
-#include "game_map.h"
-#include "player.h"
-#include "scoundrel_utils.h"
-#include "tile.h"
-#include "tile_helper.h"
+#include "scoundrel.h"
 
 namespace Scoundrel {
-  enum game_modes {GAME_PLAY, GAME_END, GAME_WIN, GAME_NEXT_LEVEL, GAME_MAP_EDIT};
-
-  //TODO: move away from all the globals.
-  const float WALK = 0.25f;
-  const float MAX_WALK = 4.f;
-  const float WALK_STOP = 4.f;
-  const int MOVE_DELTA = MAX_WALK;
-  const float JUMP_SPEED = -18.5f;
-
-  const float GRAVITY = 1.6f;
-  const float TERMINAL_VELOCITY = 15.f;
-  const int TILE_WIDTH = 32, TILE_HEIGHT = 32;
-  const int FRAMERATE_LIMIT = 60;
-  const int NUM_ANIMATIONS=20;
-
-  // Impleent a camera/view class
-  int WINDOW_WIDTH, WINDOW_HEIGHT;
-  const float CAMERA_SNAP_X = 0.2f, CAMERA_SNAP_Y = 0.23f;
-
   //I really hate all these globals. With more time I'd actually write some decent code
   GameMap* game_map;
   sf::Sprite* sprites;
@@ -61,7 +18,6 @@ namespace Scoundrel {
 
   game_modes game_mode;
   int game_start_level, current_level, total_levels;
-  std::string proc_path;
   std::list<Entity*> game_entities;
 
   float framerate = 0.f;
